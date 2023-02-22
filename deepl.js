6c807aae2654a6d683560e33316352cdacdc4d20
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-02-22 19:48:44 +0800

    Update deepl.js

--- deepl.js
+++ deepl.js
@@ -1,3 +1,3 @@
 $request.headers[":authority"] = "deep-4i3w.onrender.com";
-$request.body.replace(/source_lang=/, "source_lang=auto");
-$done({request});
+$request.body.replace(/source_lang=/g, "source_lang=auto");
+$done({});
