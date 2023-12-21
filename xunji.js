adb8fb93c5ef9098f5f11356914c6524d2537d12
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2023-12-21 09:43:30 +0800

    Create xunji.js

---
+++ xunji.js
@@ -0,0 +1,3 @@
+let headers = $request.headers;
+headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuaWQiOiJvRmRFZzFJa09mcGpZdlRZUkFKV0haSmhxY3V3IiwiaWF0IjoxNzAyODk4MjYwfQ.8YqEAsRoVPts1di3rzdE1bNxU7W0bpXy1ZkUWj3jzks';
+$done({headers});
