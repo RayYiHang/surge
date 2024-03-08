0d5cbcaa9ab0d7a2576b3fdc78cb30ba15047a79
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2024-03-08 17:56:44 +1100

    Update tradingViewCrack.js

--- tradingViewCrack.js
+++ tradingViewCrack.js
@@ -4,9 +4,9 @@ if (temp) {
   temp["pro_plan"] = "pro_premium";
   temp["plan"] = {
     billing_cycle: "m",
-    subscription_id: "730001394409785",
+    subscription_id: "730001394409783",
     is_payments_banned: false,
-    platform_product_id: "com.tradingview.pro.premium.monthly",
+    platform_product_id: "com.tradingview.pro.premium.monthly.trial",
     is_hold_period: false,
     promo_available_timestamp: null,
     merchant: "apple",
@@ -14,16 +14,16 @@ if (temp) {
     pro_plan_expire_on: 3712552491,
     is_lite_plan: false,
     is_trial_available: false,
-    next_pro_plan: null,
-    pro_plan: "pro_premium",
+    next_pro_plan: "pro_premium",
+    pro_plan: "pro_premium_trial",
     is_grace_period: false,
-    is_renewal_active: false,
+    is_renewal_active: true,
     is_renewal_paused: false,
   };
   if (temp["badges"]) {
     temp["badges"][0] = {
-      name: "pro:pro_premium",
-      verbose_name: "Premium",
+      name: "pro:pro_premium_trial",
+      verbose_name: "Premium试用",
     };
   }
   $done({ body: JSON.stringify(temp) });
