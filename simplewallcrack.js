5b6cf0f47bda6176349ffcde261f3bd6c76bc9aa
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2024-03-09 21:05:33 +1100

    Update simplewallcrack.js

--- simplewallcrack.js
+++ simplewallcrack.js
@@ -1,8 +1,6 @@
 let res = JSON.parse($response.body);
 
-if (res?.meta?.plan_restricted) {
-  res.meta.plan_restricted = false;
-  $done({ body: JSON.stringify(res) });
-} else {
-  $done({});
-}
+res["meta"] = res["meta"] || {};
+res["meta"]["plan_restricted"] = res["meta"]["plan_restricted"] ?? false;
+
+$done({ body: JSON.stringify(res) });
