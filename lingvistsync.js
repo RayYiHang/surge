8451052644d63fe29cd1ee7d962509a35b60e247
Arcadia <57169358+RayYiHang@users.noreply.github.com>
2024-03-16 15:28:17 +0800

    Update lingvistsync.js

--- lingvistsync.js
+++ lingvistsync.js
@@ -1,13 +1,17 @@
-var objc = JSON.parse($response.body);
+if ($response.body) {
+  var objc = JSON.parse($response.body);
 
-if (objc && objc["subscription"]) {
-  objc["subscription"] = {
-    status: "intro-trial",
-    expiration_ts: "9999-03-30T07:06:24+00:00",
-    trial_available: false,
-    on_hold: false,
-    trial_duration: "P1Y",
-  };
+  if (objc && objc["subscription"]) {
+    objc["subscription"] = {
+      status: "intro-trial",
+      expiration_ts: "9999-03-30T07:06:24+00:00",
+      trial_available: false,
+      on_hold: false,
+      trial_duration: "P1Y",
+    };
+  }
+
+  $done({ body: JSON.stringify(objc) });
+} else {
+  $done({ body: JSON.stringify({}) });
 }
-
-$done({ body: JSON.stringify(objc) });
